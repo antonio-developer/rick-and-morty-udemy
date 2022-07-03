c61a9578c478ebc9efce363682517e9fe11b11ab
Antonio <antoniodeveloper3@gmail.com>
2022-07-02 22:22:20 -0300

    projeto de rick and morty

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <h1>Rick and Morty</h1>
+    <FilterByStatus />
+    <FilterByName />
+    <ListCharacters />
+  </div>
+</template>
+
+<script>
+import ListCharacters from '@/components/ListCharacters';
+import FilterByStatus from '@/components/FilterByStatus';
+import FilterByName from '@/components/FilterByName';
+
+export default {
+  name: 'App',
+  components: {
+    ListCharacters,
+    FilterByStatus,
+    FilterByName,
+  },
+};
+</script>
+
+<style lang="scss">
+:root {
+  --background-body: #24282f;
+  --background-card: #3c3e44;
+  --text-white: white;
+  --text-gray: #c0c0c0;
+  --text-green: #00ff00;
+  --text-red: #ff0000;
+  --text-orange: #ff9800;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
+  text-align: center;
+}
+.container {
+  width: 980px;
+  max-width: 90%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
+}
+</style>
